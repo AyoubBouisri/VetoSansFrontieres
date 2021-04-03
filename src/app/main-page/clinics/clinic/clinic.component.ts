3e382cf9508186ed600887401017dacfdbd44637
AyoubBouisri <ayoub-mohamed.bouisri@polymtl.ca>
2021-04-03 01:03:23 -0400

    added clinics carousel

--- src/app/main-page/clinics/clinic/clinic.component.ts
+++ src/app/main-page/clinics/clinic/clinic.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ClinicStructure } from '../clinics.component';
 
 @Component({
   selector: 'app-clinic',
   templateUrl: './clinic.component.html',
   styleUrls: ['./clinic.component.scss']
 })
-export class ClinicComponent implements OnInit {
+export class ClinicComponent {
+  @Input() clinicInformation: ClinicStructure;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  
 
 }
--- src/app/main-page/clinics/clinics.component.ts
+++ src/app/main-page/clinics/clinics.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
+
+// TODO fill up the clinic structure 
+export interface ClinicStructure {
+  name: string;
+  image : string;
+
+} 
 @Component({
   selector: 'app-clinics',
   templateUrl: './clinics.component.html',
   styleUrls: ['./clinics.component.scss']
 })
-export class ClinicsComponent implements OnInit {
+export class ClinicsComponent {
 
-  constructor() { }
+  private readonly NUMBER_OF_IMAGES = 3;
+  public clinics : ClinicStructure[] = []
 
-  ngOnInit(): void {
+  constructor() {
+    this.clinics = this.getClinics();
+  }
+  
+  /**
+   * Returns all the clinics under the foundation
+   * TODO get the clinics from the database
+   * @returns 
+   */
+  getClinics() : ClinicStructure[] {
+    return [
+      {
+        name : 'MONTREAL',
+        image : this.getRandomClinicImage()
+      },
+      {
+        name : 'GATINEAU',
+        image : this.getRandomClinicImage()
+      },
+      {
+        name : 'ONTARIO',
+        image : this.getRandomClinicImage()
+      },
+      {
+        name : 'VANCOUVER',
+        image : this.getRandomClinicImage()
+      }
+    ]
   }
 
+  private getRandomClinicImage() : string {
+    let randomNbr = Math.floor(Math.random() * this.NUMBER_OF_IMAGES) + 1;
+    return "assets/clinic-images/" + randomNbr + ".png"
+  }
 }
