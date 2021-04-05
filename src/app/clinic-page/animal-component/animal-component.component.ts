70ba4f95e9415b4f7d41526e181b10a667a8cf24
AyoubBouisri <ayoub-mohamed.bouisri@polymtl.ca>
2021-04-04 22:20:23 -0400

    added animal structure

--- src/app/clinic-page/animal-component/animal-component.component.ts
+++ src/app/clinic-page/animal-component/animal-component.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+export interface animalStructure {
+  name: string,
+  type: string,
+  espece: string,
+  taille: string,
+  poids: string,
+  dateDeNaissance: string;
+  dateDInscription: string;
+  etat: string,
+  proprietaire: string
+}
 @Component({
   selector: 'app-animal-component',
   templateUrl: './animal-component.component.html',
   styleUrls: ['./animal-component.component.scss']
 })
-export class AnimalComponentComponent implements OnInit {
+export class AnimalComponentComponent {
 
-  constructor() { }
+  @Input() animalInformations: animalStructure;
+  
+  photo: string = '';
+  constructor() {
+    this.photo = this.getAnimalPhoto();
+   }
 
-  ngOnInit(): void {
+
+  getAnimalPhoto() {
+    let randomNbr = Math.floor(Math.random() * 5) + 1;
+    return "assets/animals/" + randomNbr + ".jpg" 
   }
-
 }
--- src/app/clinic-page/clinic-page.component.ts
+++ src/app/clinic-page/clinic-page.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ClinicStructure } from '../main-page/clinics/clinics.component';
 import {ActivatedRoute} from "@angular/router";
+import { animalStructure } from './animal-component/animal-component.component';
 
 @Component({
   selector: 'app-clinic-page',
   templateUrl: './clinic-page.component.html',
   styleUrls: ['./clinic-page.component.scss']
 })
-export class ClinicPageComponent implements OnInit {
+export class ClinicPageComponent {
 
   // give the initial clinic information empty values while the real clinic info is loading
   public clinicInformation: ClinicStructure = {
@@ -17,11 +18,10 @@ export class ClinicPageComponent implements OnInit {
     image: ''
   }
 
+  public animals: animalStructure[] = [];
+
   constructor(private route: ActivatedRoute) {
     this.route.params.subscribe( params => this.getClinicInformation(params.id) );
-  }
-  
-  ngOnInit(): void {
   }
 
   getClinicInformation( id: string ) : void {
@@ -32,9 +32,36 @@ export class ClinicPageComponent implements OnInit {
       number: '123',
       image: ''
     }
+
+    this.animals = this.getAnimals();
   }
 
-  getAnimals() : void {
-
+  getAnimals() : animalStructure[] {
+    // TODO replace method to call the server to get animals related to this Id.
+    return [
+      {
+        name: 'Bilou',
+        type: 'Chien',
+        espece: 'Berger Allemand',
+        taille: '1.75 cm',
+        poids: '150 lbs',
+        dateDeNaissance: '12-Fevrier-2020',
+        dateDInscription: '2 Mars 2021',
+        etat: 'Vivant',
+        proprietaire: 'Who Knows !'
+      },
+      {
+        name: 'Pupi',
+        type: 'Chat',
+        espece: 'Chat Allemand',
+        taille: '3 cm',
+        poids: '400 lbs',
+        dateDeNaissance: '12-Fevrier-2020',
+        dateDInscription: '2 Mars 2021',
+        etat: 'Vivant',
+        proprietaire: 'Maraine Ginette'
+      }
+    ]
+       
   }
 }
