d06cb430ed58651cd8f5a894e66ec0f3794b22cc
AyoubBouisri <ayoub-mohamed.bouisri@polymtl.ca>
2021-04-18 11:40:51 -0400

    .

--- client/src/app/clinic-page/clinic-page.component.ts
+++ client/src/app/clinic-page/clinic-page.component.ts
@@ -41,8 +41,9 @@ export class ClinicPageComponent {
   }
 
   getAnimals(id:string) : animalStructure[] {
-
+    this.animals = [];
     this.communicationService.getAnimalsInClinique(id).subscribe((animals : animal[]) => {
+      this.
     })
     return [
       {
